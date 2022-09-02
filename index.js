ceb9dad1903edda0a1acd82ce5f2ce3c13596b5d
Nelson <gmz_nlsn@yahoo.com>
2022-09-01 20:52:55 -0700

    complete

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+const employee = {
+  name: 'Sam'
+};
+
+const updateEmployeeWithKeyAndValue = (employee, key, value) => {
+  return {
+    ...employee,
+    [key]: value,
+  };
+}
+
+const destructivelyUpdateEmployeeWithKeyAndValue = (employee, key, value) => {
+  employee[key] = value;
+  return employee;
+}
+
+const deleteFromEmployeeByKey = (employee,key) => {
+  let newObj = {...employee};
+  delete newObj[key];
+  return newObj
+}
+
+const destructivelyDeleteFromEmployeeByKey =(employee, key)=>{
+delete employee[key]
+return employee
+}
